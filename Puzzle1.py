44de8887558c5267d63dc1f61d12814385be9dec
HackiZach <55448721+ZachO-B@users.noreply.github.com>
2020-03-31 17:05:03 -0500

    Puzzle Addition

---
+++ Puzzle1.py
@@ -0,0 +1,4 @@
+# Bakers: Pete, Perry, Paula, Pam, Penny
+# 35 Pies sold each
+# Pies: Apple, Cherry, Blueberry, Pumpkin, Peach
+# No baker sold same number of any pie
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Puzzles
+
+
+This repo was designed for engineers to practice their puzzle solving skills to assist with learning how to code. 
